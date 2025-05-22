b9f26943be63414154ed2106fcbbb91eba2e92bd
Flames-ui <anointedflames27@gmail.com>
2025-05-22 16:39:07 +0100

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), basicSsl()],
-    build: {
-        outDir: './docs',
-    },
-    base: './',
-    server: {
-        port: 5174,
-    },
-});
+  base: './', // ensures relative paths so it works correctly on Render
+  build: {
+    outDir: 'docs', // tells Vite to output static site to 'docs' folder
+  },
+}

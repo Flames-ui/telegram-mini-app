a67df7c45f33fe653cb8eaf0a29dd9dee88d5c54
Flames-ui <anointedflames27@gmail.com>
2025-05-25 00:59:26 +0100

    NetworkBadge.tsx

--- src/components/connectOverlay/NetworkBadge.tsx
+++ src/components/connectOverlay/NetworkBadge.tsx
@@ -1,45 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import getDominantColor from '../../utils/dominantColor'; // default import
 
-import { getDominantColor } from '../../utils/dominantColor';
+const NetworkBadge: React.FC = () => {
+  const [color, setColor] = useState<string>('');
 
-type Props = {
-    network: string;
-    icon: string;
-    selected: boolean;
-    callback: () => void;
-};
+  useEffect(() => {
+    getDominantColor()
+      .then((color: string) => {
+        setColor(color);
+      })
+      .catch((error: any) => {
+        console.error('Error fetching dominant color:', error);
+      });
+  }, []);
 
-const NetworkBadge: React.FC<Props> = ({
-    network,
-    icon,
-    selected,
-    callback,
-}) => {
-    const [backgroundColor, setBackgroundColor] = useState('');
-    useEffect(() => {
-        if (!selected) return;
-
-        getDominantColor(icon)
-            .then((color) => {
-                setBackgroundColor(color);
-            })
-            .catch((error) =>
-                console.error('Failed to load image color:', error)
-            );
-    }, [icon, selected]);
-
-    return (
-        <div
-            onClick={callback}
-            className={`flex flex-col items-center justify-center text-customBlackText rounded-lg p-2 
-           
-            `}
-            style={{ backgroundColor: selected ? backgroundColor : '' }}
-        >
-            <img className="w-11 h-11" src={icon} alt="" />
-            <p className="dark:text-customDarkModeTextColor">{network}</p>
-        </div>
-    );
+  return (
+    <div style={{ backgroundColor: color }}>
+      {/* Badge content */}
+      Network Badge
+    </div>
+  );
 };
 
 export default NetworkBadge;

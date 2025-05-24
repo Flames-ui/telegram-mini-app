0ae0330cf11e1cd96f10179e15ce14a264da737d
Flames-ui <anointedflames27@gmail.com>
2025-05-25 00:53:18 +0100

    types.d.ts

--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -1,10 +1,4 @@
-// Type declarations for utility modules
-declare module '../../utils/truncateText' {
-  const truncateText: (text: string, maxLength: number) => string;
-  export default truncateText;
-}
-
 declare module '../../utils/dominantColor' {
-  const getDominantColor: (imageUrl: string) => Promise<string>;
+  const getDominantColor: () => Promise<string>;
   export default getDominantColor;
 }

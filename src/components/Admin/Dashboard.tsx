c7b78199a9a2498e4af0263253212141ddc7f424
Flames-ui <anointedflames27@gmail.com>
2025-05-25 02:34:18 +0100

    Dashboard.tsx

--- src/components/Admin/Dashboard.tsx
+++ src/components/Admin/Dashboard.tsx
@@ -1,7 +1,7 @@
-export default function AdminDashboard() {
-  return (
-    <div className="admin-dashboard">
-      <h1>Admin Dashboard</h1>
-    </div>
-  );
-}
+import React from 'react';
+
+const AdminDashboard = () => {
+  return <h2>Admin Dashboard</h2>;
+};
+
+export default AdminDashboard;

9b9ec6505e197d01e073befa0653f4eda2248ffd
Flames-ui <anointedflames27@gmail.com>
2025-05-25 01:53:33 +0100

    Admin.tsx

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './AdminApp'
-import './admin.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './AdminApp';
+import './admin.css';
 
-ReactDOM.createRoot(document.getElementById('admin-root')!).render(
+ReactDOM.createRoot(
+  document.getElementById('admin-root') as HTMLElement
+).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+);

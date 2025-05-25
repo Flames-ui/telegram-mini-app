718119ebf1034abd3172de61d35d0eeda2a67626
Flames-ui <anointedflames27@gmail.com>
2025-05-25 01:09:08 +0100

    ConnectOverlay.tsx

--- src/components/connectOverlay/ConnectOverlay.tsx
+++ src/components/connectOverlay/ConnectOverlay.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import truncateText from '../../utils/truncateText';
+import { truncateText } from '../../utils/truncateText';
 
 interface ConnectOverlayProps {
-  text: string; // example prop, adjust as needed
+  slideAnimation: string;
+  close: () => void;
+  onConnect: () => void;
+  account: string | null;
 }
 
-const ConnectOverlay: React.FC<ConnectOverlayProps> = ({ text }) => {
+const ConnectOverlay: React.FC<ConnectOverlayProps> = ({ slideAnimation, close, onConnect, account }) => {
+  const someText = "This is a sample long text that needs to be truncated to look better in UI.";
+
   return (
-    <div>
-      {/* other JSX */}
-      <p className="dark:text-customDarkModeTextColor">
-        {truncateText(text, 100)}
-      </p>
-      {/* other JSX */}
+    <div className={`connect-overlay ${slideAnimation}`}>
+      <div className="content">
+        <p className="dark:text-customDarkModeTextColor">
+          {truncateText(someText, 100)}
+        </p>
+        <button onClick={onConnect}>Connect Wallet</button>
+        <button onClick={close}>Close</button>
+        {account && <p>Connected account: {account}</p>}
+      </div>
     </div>
   );
 };

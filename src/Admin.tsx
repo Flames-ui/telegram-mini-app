959007b38d841a3a35a3e978ebd4f0706bdc323c
Flames-ui <anointedflames27@gmail.com>
2025-05-25 01:41:33 +0100

    Admin.tsx

---
+++ src/Admin.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './AdminApp'
+import './admin.css'
+
+ReactDOM.createRoot(document.getElementById('admin-root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>

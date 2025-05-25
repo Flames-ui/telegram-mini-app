0621e2bc4a871f8751d88778225691e63d839d76
Flames-ui <anointedflames27@gmail.com>
2025-05-25 02:08:54 +0100

    AdminApp.tsx

--- src/AdminApp.tsx
+++ src/AdminApp.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AdminLogin, AdminDashboard } from './components/Admin'; // Fixed path
+import { AdminLogin, AdminDashboard } from '../components/Admin'; // Fixed path
 
 export default function AdminApp() {
   return (

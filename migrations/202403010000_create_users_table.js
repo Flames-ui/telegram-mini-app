2d80e28e71b0bcdb7189dca327389f33fe32f169
Flames-ui <anointedflames27@gmail.com>
2025-05-30 08:25:18 +0100

    202403010000_create_users_table.js

--- migrations/202403010000_create_users_table.js
+++ migrations/202403010000_create_users_table.js
@@ -2,6 +2,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('users', (table) => {
     table.increments('id').primary();
     table.string('email').unique().notNullable();
+    table.string('name').notNullable();
     table.string('password_hash').notNullable();
     table.boolean('is_admin').defaultTo(false);
     table.timestamps(true, true);

8aa6430adb2550f2e0676487cfd4cb34651a10c0
Flames-ui <anointedflames27@gmail.com>
2025-05-30 08:36:34 +0100

    database.js

--- config/database.js
+++ config/database.js
@@ -3,9 +3,7 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: process.env.DATABASE_URL,
-  migrations: {
-    tableName: 'migrations'
-  }
+  pool: { min: 0, max: 10 }
 });
 
 module.exports = db;

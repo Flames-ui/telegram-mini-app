99d0384fc9a7d80bd01a4a1d42fe3a00d94366b6
Flames-ui <anointedflames27@gmail.com>
2025-05-30 08:35:05 +0100

    storage.js

--- service/storage.js
+++ service/storage.js
@@ -1,11 +1,10 @@
 const fs = require('fs');
 const path = require('path');
-const sharp = require('sharp');
 const { v4: uuidv4 } = require('uuid');
 
 class StorageService {
   constructor() {
-    this.basePath = process.env.STORAGE_PATH;
+    this.basePath = path.join(__dirname, '../../storage');
   }
 
   async saveFile(file, subfolder = '') {
@@ -15,32 +14,6 @@ class StorageService {
     
     await fs.promises.mkdir(path.dirname(filePath), { recursive: true });
     await fs.promises.rename(file.path, filePath);
-    
-    return {
-      originalName: file.originalname,
-      fileName,
-      filePath,
-      size: file.size,
-      mimetype: file.mimetype
-    };
-  }
-
-  async processImage(file, options = {}) {
-    const { width, height, quality } = options;
-    const processedBuffer = await sharp(file.path)
-      .resize(width, height, { fit: 'inside' })
-      .jpeg({ quality: quality || 80 })
-      .toBuffer();
-      
-    return this.saveBuffer(processedBuffer, 'processed', 'jpg');
-  }
-
-  async saveBuffer(buffer, subfolder, extension) {
-    const fileName = `${uuidv4()}.${extension}`;
-    const filePath = path.join(this.basePath, subfolder, fileName);
-    
-    await fs.promises.mkdir(path.dirname(filePath), { recursive: true });
-    await fs.promises.writeFile(filePath, buffer);
     
     return filePath;
   }
